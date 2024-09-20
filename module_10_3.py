2e32abd7ad4da395768f4b2fc535d5f7a44fc184
viktorvs-dotcom <viktorvs1987@gmail.com>
2024-09-20 19:40:21 +0300

    module_10_3.py

---
+++ module_10_3.py
@@ -0,0 +1,45 @@
+from threading import Thread, Lock
+from random import randint
+from time import sleep
+
+
+class Bank(Thread):
+    lock = Lock()
+
+    def __init__(self):
+        super().__init__()
+        self.balance = 100
+        self.lock = Lock()
+
+    def deposit(self):
+        for i in range(5):
+            with self.lock:
+                cash = randint(50, 500)
+                self.balance += cash
+                print(f"Пополнение на: {cash}. Текущий баланс{self.balance}")
+            if self.balance <= 500:
+                sleep(0.001)
+
+    def take(self):
+        for i in range(5):
+            with self.lock:
+                cash = randint(50, 500)
+                print(f"Запрос на снятие {cash}")
+                if self.balance >= cash:
+                    self.balance -= cash
+                    print(f"Снятие: {cash} запрос выполнен. Баланс:{self.balance}")
+                else:
+                    print(f"Запрос откланён, недостаточно средств")
+
+
+bk = Bank()
+
+th1 = Thread(target=Bank.deposit, args=(bk,))
+th2 = Thread(target=Bank.take, args=(bk,))
+
+th1.start()
+th2.start()
+th1.join()
+th2.join()
+
+print(f"Итоговый баланс: {bk.balance}")
