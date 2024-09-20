062a00d3653b3fc6ff8432cf93eea1e5e9e3fb81
viktorvs-dotcom <viktorvs1987@gmail.com>
2024-09-20 23:50:10 +0300

    module_10_3.py

--- module_10_3.py
+++ module_10_3.py
@@ -12,7 +12,7 @@ class Bank(Thread):
         self.lock = Lock()
 
     def deposit(self):
-        for i in range(5):
+        for i in range(100):
             with self.lock:
                 cash = randint(50, 500)
                 self.balance += cash
@@ -21,7 +21,7 @@ class Bank(Thread):
                 sleep(0.001)
 
     def take(self):
-        for i in range(100): 
+        for i in range(5):# по условиям - 100 в наличии - 5
             with self.lock:
                 cash = randint(50, 500)
                 print(f"Запрос на снятие {cash}")
